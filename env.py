a86791d3724d19fd306abdfa520aa9a43f0c171f
Vadim Avkhimenia <avkhimen@ualberta.ca>
2024-01-10 06:35:16 +0000

    Adding functions to add to env

--- env.py
+++ env.py
@@ -4,3 +4,7 @@ import numpy as np
 class TSEnv():
     def __init__(self, ts):
         self.ts = ts
+        # obs_dim = env.observation_space.shape[0]
+        # action_dim = env.action_space.n
+        # self.env.step(action)
+        # self.env.reset(seed=self.seed)
