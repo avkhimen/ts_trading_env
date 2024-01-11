d664e7438f2e1956755fbb644310ad34e66cf144
Vadim Avkhimenia <avkhimen@ualberta.ca>
2024-01-11 06:59:36 +0000

    Adding tests

--- env.py
+++ env.py
@@ -9,8 +9,15 @@ class TSEnv():
         self.observation_space = np.zeros((obs_dim, ))
         self.action_space = gymnasium.spaces.Discrete(action_dim, seed=self.seed)
 
-    def reset(self, seed=self.seed):
+    def reset(self):
+        seed=self.seed
         pass
     
     def step(self, action):
         pass
+
+# Test
+env = TSEnv([1,2,3], 3, 2, 32)
+print(env.observation_space)
+print(env.action_space)
+print(env.action_space.sample())
